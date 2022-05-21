d0a2df4422dff9b80e18bd26235966c75c4f20c3
NishikinoSky <67897612+NishikinoSky@users.noreply.github.com>
2022-05-21 22:10:37 +0800

    Add files via upload

--- front/js/index.js
+++ front/js/index.js
@@ -60,45 +60,45 @@ function myFunction() {
     document.getElementById("rtbox").innerHTML = y + "-" + mon + "-" + d + " " + w;
     document.getElementById("lttime").innerHTML = ampm + ' ' + h + ":" + m + ":" + s;
 
-    //获取元素
-    var txt = document.querySelector('#txt');
-    var btn = document.querySelector('#add');
-    var ul = document.querySelector('.notes');
-    var colors = ['#ef9a9a', '#F48FB1', '#CE93D8', '#B39DDB', '#9FA8DA', '#90CAF9', '#81D4FA', '#80DEEA', '#80CBC4', '#A5D6A7', '#C5E1A5', '#FFCC80', '#FFAB91', '#BCAAA4', '#B0BEC5'];
-    //注册事件
-    btn.onclick = function () {
-        txt.focus();
-        if (ul.children.length < 8 && txt.value != '') {
+    // //获取元素
+    // var txt = document.querySelector('#txt');
+    // var btn = document.querySelector('#add');
+    // var ul = document.querySelector('.notes');
+    // var colors = ['#ef9a9a', '#F48FB1', '#CE93D8', '#B39DDB', '#9FA8DA', '#90CAF9', '#81D4FA', '#80DEEA', '#80CBC4', '#A5D6A7', '#C5E1A5', '#FFCC80', '#FFAB91', '#BCAAA4', '#B0BEC5'];
+    // //注册事件
+    // btn.onclick = function () {
+    //     txt.focus();
+    //     if (ul.children.length < 8 && txt.value != '') {
 
-            //创建元素
-            var li = document.createElement('li');
+    //         //创建元素
+    //         var li = document.createElement('li');
 
-            li.style.backgroundColor = colors[parseInt(Math.random() * (colors.length - 1))];
-            //添加元素
-            ul.appendChild(li);
-            li.innerHTML = txt.value + "<p>" + h + ":" + m + " " + ampm + "</p>" + "<a href='javascript:;' style='background:" + li.style.backgroundColor + "' >Delete</a>";
-            txt.value = '';
-            txt.focus();
-            //删除元素
-            var as = document.querySelectorAll('a');
-            for (var i = 0; i < as.length; i++) {
-                as[i].onclick = function () {
-                    ul.removeChild(this.parentNode);
-                    txt.focus();
-                }
-            }
+    //         li.style.backgroundColor = colors[parseInt(Math.random() * (colors.length - 1))];
+    //         //添加元素
+    //         ul.appendChild(li);
+    //         li.innerHTML = txt.value + "<p>" + h + ":" + m + " " + ampm + "</p>" + "<a href='javascript:;' style='background:" + li.style.backgroundColor + "' >Delete</a>";
+    //         txt.value = '';
+    //         txt.focus();
+    //         //删除元素
+    //         var as = document.querySelectorAll('a');
+    //         for (var i = 0; i < as.length; i++) {
+    //             as[i].onclick = function () {
+    //                 ul.removeChild(this.parentNode);
+    //                 txt.focus();
+    //             }
+    //         }
 
-        } else if (ul.children.length == 8) {
-            txt.value = '';
-            txt.setAttribute('placeholder', '只能添加8个便签哦！');
-            txt.focus();
+    //     } else if (ul.children.length == 8) {
+    //         txt.value = '';
+    //         txt.setAttribute('placeholder', '只能添加8个便签哦！');
+    //         txt.focus();
 
-        }
-        else if (txt.value == '') {
-            txt.setAttribute('placeholder', '请输入内容...');
-            txt.focus();
-        }
-    }
+    //     }
+    //     else if (txt.value == '') {
+    //         txt.setAttribute('placeholder', '请输入内容...');
+    //         txt.focus();
+    //     }
+    // }
 
 }
 //递归调用 每秒调用一次
@@ -108,24 +108,141 @@ function getFocus() {
     document.getElementById('seaid').focus();
 }
 
+// codemirror构建代码框
+var editor = CodeMirror.fromTextArea(document.getElementById("txt"), {
+    mode: "text/x-java", //实现Java代码高亮
+    mode: "text/x-c++src",
+    lineNumbers: true,//是否显示每一行的行数
+    indentUnit: 4, //缩进单位为4
+    matchBrackets: true, //括号匹配
+    styleActiveLine: true, //当前行背景高亮
+    readOnly: false,//只读
+    lineWrapping: true,	//代码折叠
+    foldGutter: true,
+    smartIndent: true,//智能缩进
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    lineWrapping: true, //自动换行
+    theme: 'ayu-mirage', //编辑器主题
+});
+//var height = 650;
+editor.setSize('100%', '100%');
 
 var btnn = document.querySelector('#note');
 var div = document.querySelector('.btm');
 var flag1 = true;
+var ifDownload = false;
 function addFocus() {
     if (!flag1) {
-        div.style.transform = 'translateX(0px)';
+        div.style.transform = 'translateX(0px)'; // 隐藏Codeblock
         flag1 = true;
         txt.blur();
     } else {
-        div.style.transform = 'translateX(1300px)';
+        div.style.transform = 'translateX(1300px)';// 展开Codeblock
         txt.focus();
         flag1 = false;
     }
 }
 btnn.onclick = function () {
+    ifDownload = true;
     addFocus();
+
 }
+
+function keepFocus() {
+    if (flag1) {
+        div.style.transform = 'translateX(1300px)';
+        txt.focus();
+    }
+}
+
+function clickOpen() {
+    ifDownload = false;
+    keepFocus();
+    openFile();
+}
+
+function clickSave() {
+    var content = editor.getValue();
+    var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+    //创建一个blob的对象连链接
+    var url = window.URL.createObjectURL(blob);
+    // 创建一个链接元素，是属于 a 标签的链接元素
+    var link = document.createElement('a');
+    // 把blob的对象链接赋值给新创建的这个 a 链接
+    link.href = url;
+    // 设置下载的属性（所以使用的是download），这个是a标签的一个属性
+    link.setAttribute('download', "test.cmm");
+    link.click();
+}
+
+function clickRun() {
+    keepFocus();
+    if (ifDownload) {
+        // 保存文件
+    }
+}
+
+function clickTerminal() {
+    var shell = require('shelljs');//这里要下载shell插件
+    module.export = {
+        open: function () {
+            //生成run.sh脚本
+            var comd = 'echo >> run.sh \#/bin/sh cd ~/Documents/2022_SPRSUM/compiler/myCompiler/src\n make test';
+            shell.exec(comd, { silent: true });
+            //给脚本增加运行权限
+            shell.exec('chmod +x run.sh', { silent: true });
+            //打开运行
+            shell.exec('open -a Terminal run.sh');
+        }
+    }
+    // var comd = 'echo >> run.sh \#/bin/sh ~/Documents/2022_SPRSUM/compiler/myCompiler/src\n make test';
+    // shell.exec(comd, { silent: true });
+    // //给脚本增加运行权限
+    // shell.exec('chmod +x run.sh', { silent: true });
+    // //打开运行
+    // shell.exec('open -a Terminal run.sh');
+}
+
+function openFile() {
+    $('#fileInput').trigger('click');
+    document.getElementById('fileInput').addEventListener('change', function selectedFileChanged() {
+        if (this.files.length == 0) {
+            console.log('Choose file nia~');
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = function fileReadCompleted() {
+            //当读取完成时，内容只在`reader.result`中
+            console.log(reader.result);
+            var content = reader.result;
+            editor.setValue(content);
+        };
+        reader.readAsText(this.files[0]);
+    });
+    return;
+}
+
+
+// function saveFile() {
+
+//     var strFile = "~/Documents/2022_SPR\&SUM/compiler/myCompiler";
+//     var objFSO = new ActiveXObject("Scripting.FileSystemObject");
+//     // 检查文件是否存在
+//     if (!objFSO.FileExists(strFile)) {
+//         // 创建文本文件
+//         var objStream = objFSO.CreateTextFile(strFile, true);
+//         objStream.Write(document.getElementById("txt").innerText);
+//         //document.write("创建文本文件: " + strFile + "<br>");
+//         objStream.Close(); // 关闭文件
+//         alert("ok");
+//     }
+//     else {
+//         alert("文本文件: " + strFile + "已经存在<br>");
+//     }
+// }
+
+
+
 
 // function myTime() {
 //     let time = new Date();
